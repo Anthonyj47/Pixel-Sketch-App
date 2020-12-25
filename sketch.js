24b74f3c70ab6ee66b99b1c63d7c83ecc94c202d
Anthonyj47 <68197083+Anthonyj47@users.noreply.github.com>
2020-12-25 16:40:02 +0200

    mode is slightly more gay

--- sketch.js
+++ sketch.js
@@ -5,20 +5,41 @@ container.classList.add("container")
 let body = document.getElementById("body");
 body.appendChild(container)
 
-//Generating cells
+let grid_sct = document.createElement('p');
+grid_sct.classList.add("grid-sct")
+grid_sct.innerHTML = "Circle Grid";
+body.appendChild(grid_sct)
+
+//Generating cell grid
 for(let i = 0; i < (16*16); i++){
     let cell = document.createElement('div');
     cell.classList.add('cell');
+
+    //Color grids
     cell.addEventListener('click', (e)=> {
         if(e.ctrlKey === true){
-            cell.setAttribute('style','background-color: #fff');
+            cell.style.backgroundColor = "#fff";
         }
         else{
-            cell.setAttribute('style','background-color: #333');
+            cell.style.backgroundColor = "#333";
+        }
+    });    
+    grid_sct.addEventListener('click', (e)=> {
+        if (grid_sct.innerHTML == "Circle Grid") {
+            grid_sct.innerHTML == "Block Grid";
+        } else {
+            grid_sct.innerHTML == "Circle Grid";
         }
     });
     container.appendChild(cell);
 }
+
+//Color grids
+for(let i = 0; i < (16*16); i++){
+
+}
+
+
 
 for (let index = 0; index < 1000; index++) {
     console.log("Weltuntergang");
