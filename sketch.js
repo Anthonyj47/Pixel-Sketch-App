eb99a52676bd1b14027925b175a877de68b6f956
Anthonyj47 <68197083+Anthonyj47@users.noreply.github.com>
2020-12-25 20:03:35 +0200

    Finished most UI

--- sketch.js
+++ sketch.js
@@ -5,10 +5,14 @@ container.classList.add("container")
 let body = document.getElementById("body");
 body.appendChild(container)
 
+
+//Nav Buttons
+let grid_sct = document.querySelector(".grid-sct");
 //Generating cell grid
 for(let i = 0; i < (16*16); i++){
     let cell = document.createElement('div');
     cell.classList.add('cell');
+    container.appendChild(cell);
 
     //Color grids
     cell.addEventListener('click', (e)=> {
@@ -19,12 +23,7 @@ for(let i = 0; i < (16*16); i++){
             cell.style.backgroundColor = "#333";
         }
     });    
-    container.appendChild(cell);
 }
-
-
-
-
 
 
 for (let index = 0; index < 1000; index++) {
